559925baf4c9aeaead2d8fd9f4dfc6225be68543
Sarah <sarahlchavez@gmail.com>
2021-09-04 15:12:37 -0700

    commit tree bbf77d2eefa3d8aff188576271943213f01f0fe8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+// Declare a variable in global scope called customerName using the var keyword:
+var customerName = "bob";
+
+// Write a function that access the global customerName variable and uppercase it:
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Write a function that when called, declares a variable called bestCustomer in
+// global scope and assigns it to be 'not bob' 
+
+var bestCustomer;
+
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+}
+
+// Change bestCustomer variable
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope called
+// leastFavoriteCustomer. Assign it an initial value.
+// Then write a function called changeLeastFavoriteCustomer()
+// that attempts to change that constant
+
+const leastFavoriteCustomer = 'definitely bob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'perhaps bob';
+}
+
+
+
+
